460f3c25e710e0fc033bc8da30f3cb8f975d6d94
AZ <47402666+realazaber@users.noreply.github.com>
2023-07-25 21:11:10 +0200

    Added theme colors

--- crypto-traders/tailwind.config.js
+++ crypto-traders/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        secondary: "#14171a",
+        accent: "#333333",
+      },
+    },
   },
   plugins: [],
 };
