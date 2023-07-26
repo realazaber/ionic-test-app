4b372ec56513c957b36b9a7376d243040a69bcad
AZ <47402666+realazaber@users.noreply.github.com>
2023-07-26 15:19:08 +0200

    Update app.routes.ts

--- crypto-traders/src/app/app.routes.ts
+++ crypto-traders/src/app/app.routes.ts
@@ -3,11 +3,16 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () =>
+      import('./pages/home/home.page').then((m) => m.HomePage),
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },
+  {
+    path: 'hiring',
+    loadComponent: () => import('./pages/hiring/hiring.page').then( m => m.HiringPage)
   },
 ];
