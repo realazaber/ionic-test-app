972b1c350e62eb09dbeda15049ba10eab26dbe44
AZ <47402666+realazaber@users.noreply.github.com>
2023-07-26 15:19:46 +0200

    Mobile and Desktop nav imported to app component

--- crypto-traders/src/app/app.component.ts
+++ crypto-traders/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { NavComponent } from './components/nav/nav.component';
+import { MobileNavComponent } from './components/mobile-nav/mobile-nav.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, NavComponent, MobileNavComponent],
 })
 export class AppComponent {
   constructor() {}
